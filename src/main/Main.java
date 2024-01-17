e378cb76d47fa38eef9c58ffa5d19beb86dd536a
JotredDev <s12jtrec@uni-bonn.de>
2024-01-17 22:53:18 +0100

    Added files

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,5 +6,10 @@ public class Main {
 		
 		System.out.println("hello");
 		
+		int[] test = new int[] {12, 24, 54, 60, 102, 120};
+		TextConverter textConverter = new TextConverter(4);
+		System.out.println(textConverter.rawNumberFormat(test, 3));
+		
+		System.out.println(textConverter.rawNumberFormat(test));
 	}
 }
